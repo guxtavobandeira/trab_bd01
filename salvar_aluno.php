1abb7c12fa99bf2efe0a7404653996c4720a603c
Juniorx <111861420+JuniorHora@users.noreply.github.com>
2023-10-22 12:59:55 -0300

    Update salvar_aluno.php

--- salvar_aluno.php
+++ salvar_aluno.php
@@ -1,17 +1,19 @@
 <?php
 include 'conexao.php';
 
-	$nome_bib = $_POST['nome_bib'];
-	$livro_bib = $_POST['livro_bib'];
+	$nome_aluno = $_POST['nome_aluno'];
+	$curso_aluno = $_POST['curso_aluno'];
+	$sexo_aluno = $_POST['sexo_aluno'];
+	$serie = $_POST['serie'];
+	$livro_aluno = $_POST['livro_aluno'];
 
-    $sql = mysqli_query($conexaoBD, "INSERT INTO bibliotecaria(nome_bib, livro_bib) 
-	VALUES ('$nome_bib', '$livro_bib')");
+	$sql = mysqli_query($conexaoBD, "INSERT INTO aluno(nome_aluno, curso_aluno, sexo_aluno, serie, livro_aluno)
+	VALUES ('$nome_aluno', '$curso_aluno', '$sexo_aluno', '$serie', '$livro_aluno')");
 	
-
-   // $sql2 = mysqli_query($conexaoBD, "INSERT INTO aluno_bibli(fk_bibliotecaria_cod_bib) 
-    //VALUES ('$cod_bib')");
+	//$sql2 = mysqli_query($conexaoBD, "INSERT INTO aluno_bibli(fk_Aluno_cod_aluno)
+	//VALUES ('$cod_aluno')");
 	
 	if ($sql) {
-		header('location: bibliotecaria.php');
+		header('location: aluno.php');
 	}
 ?>
