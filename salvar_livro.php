bf94fe32d426f2e0e7b55d7700c885973a5d5401
Juniorx <111861420+JuniorHora@users.noreply.github.com>
2023-10-22 12:07:44 -0300

    Update salvar_livro.php

--- salvar_livro.php
+++ salvar_livro.php
@@ -3,13 +3,14 @@ include 'conexao.php';
 
 	$titulo = $_POST['titulo'];
 	$autor = $_POST['autor'];
-	$cod_livro = $_POST['cod_livro'];
 	$data_entrega = $_POST['data_entrega'];
 	$data_dev = $_POST['data_dev'];	
 
-	$sql = mysqli_query($conexaoBD, "INSERT INTO livro(titulo, autor, cod_livro, data_entrega, data_dev) 
-	VALUES ('$titulo', '$autor', '$cod_livro', '$data_entrega', '$data_dev')");
+	$sql = mysqli_query($conexaoBD, "INSERT INTO livro(titulo, autor, data_entrega, data_dev) 
+	VALUES ('$titulo', '$autor', '$data_entrega', '$data_dev')");
 	
+    //$sql2 = mysqli_query($conexaoBD, "INSERT INTO livro_bibli(fk_livro_cod_livro) 
+	//VALUES ('$cod_livro')");
 	if ($sql) {
 		header('location: livro.php');
 	}
